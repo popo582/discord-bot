3eaf41294c14e8a9cbbf7d8e07b3413785fd8de3
popo582 <popo220617@gmail.com>
2026-03-30 22:10:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 load_dotenv()  # loads .env locally if you have it
 
-TOKEN = os.getenv("DISCORD_TOKEN")  # make sure .env or Railway variable matches
+TOKEN = os.getenv("TOKEN")  # make sure .env or Railway variable matches
 
 intents = discord.Intents.default()
 intents.message_content = True
